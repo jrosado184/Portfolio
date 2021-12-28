500ac30f45bfbde45460a7b05570989b4b58c5fa
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2021-12-27 23:48:09 -0500

    header complete with dark mode functionality

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,41 +1,99 @@
-import React from "react";
-import github from "./assets/github.png";
-import linkedin from "./assets/linkedin.png";
-import instagram from "./assets/instagram.png";
-import twitter from "./assets/twitter.png";
+import React, { useState } from "react";
+import github from "../assets/github.png";
+import linkedin from "../assets/linkedin.png";
+import instagram from "../assets/instagram.png";
+import twitter from "../assets/twitter.png";
+import whitegh from "../assets/whitegh.png";
+import { Switch } from "@headlessui/react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [dark, setDark] = useState(false);
+
   return (
-    <div className="w-full h-48 border-b-2 border-black ">
+    <div
+      className={
+        !dark
+          ? "w-full h-48 border-b-2 border-black"
+          : "w-full h-48 border-b-2 border-white bg-stone-900"
+      }
+    >
       <div className="display: flex flex space-x-4 ">
-        <nav className="my-40 ml-6">About</nav>
-        <nav className="my-40">Projects</nav>
-        <nav className="my-40">Resume</nav>
-        <nav className="my-40">Contact</nav>
+        <Link
+          to="/about"
+          className={!dark ? "my-40 ml-6" : "text-white my-40 ml-6"}
+        >
+          About
+        </Link>
+        <Link
+          to="/projects"
+          className={!dark ? "my-40 ml-6" : "text-white my-40 ml-6"}
+        >
+          Projects
+        </Link>
+        <Link
+          to="/resume"
+          className={!dark ? "my-40 ml-6" : "text-white my-40 ml-6"}
+        >
+          Resume
+        </Link>
+        <Link
+          to="/contact"
+          className={!dark ? "my-40 ml-6" : "text-white my-40 ml-6"}
+        >
+          Contact
+        </Link>
         <div className="w-full h-48 display: flex justify-end my-36 ">
           <a href="https://github.com/jrosado184" target="_blank">
-            <img className="w-8 h-8 mr-4 my-1.5" src={github} alt="github" />
+            {!dark ? (
+              <img className="w-8 h-8 mr-4 my-1.5" src={github} alt="github" />
+            ) : (
+              <img className="w-8 h-8 mr-4 my-1.5" src={whitegh} />
+            )}
           </a>
           <a
             href="https://www.linkedin.com/in/javier-rosado-2974721b5/"
             target="_blank"
+            rel="noreferrer"
           >
-            <img className="mr-4 w-12 h-12" src={linkedin} alt="github" />
-          </a>
-          <a href="https://www.instagram.com/_papijavy/" target="_blank">
             <img
-              className="w-7 h-7 mr-4 w- h-3 my-2"
-              src={instagram}
+              className="mr-4 w-12 h-12"
+              src={linkedin}
               alt="github"
+              rel="noreferrer"
             />
           </a>
-          <a href="https://twitter.com/Javier_E_Rosado" target="_blank">
+          <a
+            href="https://www.instagram.com/_papijavy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="w-7 h-7 mr-4 my-2" src={instagram} alt="github" />
+          </a>
+          <a
+            href="https://twitter.com/Javier_E_Rosado"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img
               className="w-7 h-7 mr-4 w-9 h-9 my-1"
               src={twitter}
               alt="github"
             />
           </a>
+          <Switch
+            checked={dark}
+            onChange={setDark}
+            className={`${
+              dark ? "bg-blue-400" : "bg-gray-400"
+            } relative inline-flex items-center h-6 rounded-full w-11 mr-16 my-3`}
+          >
+            <span
+              className={`${
+                dark ? "translate-x-6" : "translate-x-1"
+              } inline-block w-4 h-4 transform bg-white rounded-full`}
+            />
+          </Switch>
         </div>
       </div>
     </div>
